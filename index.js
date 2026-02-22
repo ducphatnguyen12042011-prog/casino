8172002bc79b86f41a371f0b7949d1831baff4de
ducphatnguyen12042011-prog <ducphatnguyen12042011@gmail.com>
2026-02-22 22:50:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+require('./bot_cado.js');
+require('./bot_taixiu.js');
+require('./bot_shop.js');
+require('./bot_lookup.js');
+require('./bot_admin.js');
+require('./bot_bxh.js');
+console.log("🚀 Tất cả các hệ thống Bot đã được khởi động!");
