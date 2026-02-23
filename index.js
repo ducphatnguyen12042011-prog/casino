7998b512706d780631b3f4802c956d276e0e2406
ducphatnguyen12042011-prog <ducphatnguyen12042011@gmail.com>
2026-02-23 17:04:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// Xuất prisma để các file bot_taixiu.js, bot_cado.js... có thể dùng chung
+global.prisma = prisma; 
+
+// Import các hệ thống bot
 require('./bot_cado.js');
 require('./bot_taixiu.js');
 require('./bot_shop.js');
 require('./bot_lookup.js');
 require('./bot_admin.js');
 require('./bot_bxh.js');
+
 console.log("🚀 Tất cả các hệ thống Bot đã được khởi động!");
+
+// Xử lý khi tắt bot để ngắt kết nối database an toàn
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit();
+});
